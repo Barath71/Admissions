e6e875513b8c054d273637a0cde9142fd8b9e31f
Barath71 <bk116665@gmail.com>
2025-06-24 11:49:29 +0530

    after changing something

--- submit.php
+++ submit.php
@@ -40,16 +40,17 @@ if (!move_uploaded_file($photo_tmp, $photo_path)) {
 }
 
 // Collect form data
-$first_name = $_POST['firstName'] ?? '';
-$last_name = $_POST['lastName'] ?? '';
-$gender = $_POST['gender'] ?? '';
-$email = $_POST['email'] ?? '';
-$phone = $_POST['phone'] ?? '';
-$dob = $_POST['dob'] ?? '';
-$address = $_POST['address'] ?? '';
-$group = $_POST['groupSelect'] ?? '';
-$total_marks = $_POST['marks'] ?? '';
-$course = $_POST['courseSelect'] ?? '';
+$first_name    = htmlspecialchars($_POST['firstName'] ?? '', ENT_QUOTES, 'UTF-8');
+$last_name     = htmlspecialchars($_POST['lastName'] ?? '', ENT_QUOTES, 'UTF-8');
+$gender        = htmlspecialchars($_POST['gender'] ?? '', ENT_QUOTES, 'UTF-8');
+$email         = htmlspecialchars($_POST['email'] ?? '', ENT_QUOTES, 'UTF-8');
+$phone         = htmlspecialchars($_POST['phone'] ?? '', ENT_QUOTES, 'UTF-8');
+$dob           = htmlspecialchars($_POST['dob'] ?? '', ENT_QUOTES, 'UTF-8');
+$address       = htmlspecialchars($_POST['address'] ?? '', ENT_QUOTES, 'UTF-8');
+$group         = htmlspecialchars($_POST['groupSelect'] ?? '', ENT_QUOTES, 'UTF-8');
+$total_marks   = htmlspecialchars($_POST['marks'] ?? '', ENT_QUOTES, 'UTF-8');
+$course        = htmlspecialchars($_POST['courseSelect'] ?? '', ENT_QUOTES, 'UTF-8');
+
 
 // Insert into DB
 $sql = "INSERT INTO applications 
